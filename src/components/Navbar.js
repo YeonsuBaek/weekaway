76d68d81e9112b8008ef35ec34568d7ce72cc2cb
YeonsuBaek <soothbugger@gmail.com>
2023-01-25 22:41:39 +0900

    feat: NavBar 컴포넌트 작업

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { BsChatSquareDots } from 'react-icons/bs';
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaGooglePlusG,
+  FaInstagram,
+  FaBars,
+} from 'react-icons/fa';
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className="w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80">
+      <ul className="hidden px-4 sm:flex">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="#gallery">Gallery</a>
+        </li>
+        <li>
+          <a href="#deals">Deals</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="flex justify-between">
+        <FaFacebookF className="mx-4" />
+        <FaTwitter className="mx-4" />
+        <FaGooglePlusG className="mx-4" />
+        <FaInstagram className="mx-4" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
